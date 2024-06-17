a46990847eb32109ad2188091bfde2696353976f
ihza.poli <ihza.poli@mandalafinance.com>
2024-06-17 14:45:42 +0700

    chore(deployment): update database

--- ecommerce_project/settings.py
+++ ecommerce_project/settings.py
@@ -80,16 +80,6 @@ WSGI_APPLICATION = 'ecommerce_project.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/5.0/ref/settings/#databases
 
-# DATABASES = {
-#     'default': {
-#         'ENGINE': 'django.db.backends.postgresql',
-#         'NAME': 'ecommerce',
-#         'USER': 'postgres',
-#         'PASSWORD': 'postgres',
-#         'HOST': 'localhost',
-#         'PORT': '5432',
-#     }
-# }
 DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.postgresql',
